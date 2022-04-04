1536c96e98bee2aa32cfe67e981d4506142630c2
Juan David <juan.reyes@paxzu.co>
2022-04-04 16:18:52 -0500

    avance ultimas secciones

--- js/main.js
+++ js/main.js
@@ -1,22 +1,40 @@
 $(document).ready(function(){
 
+    const body = document.querySelector('body');
+
+    $('.btn_navbar_menu').click(function(){
+        $('.nav_kawak').toggleClass('active_navbar_mobile');
+        $('body').toggleClass('scrollLock');
+    });
+
     function swiperSliderAreas(){
         const swiper = new Swiper(".sliderAreas", {
+            spaceBetween: 20,
             pagination: {
               el: ".sliderAreas .swiper-pagination",
             },
             navigation: {
                 nextEl: ".cont_slider_areas .swiper-button-next",
                 prevEl: ".cont_slider_areas .swiper-button-prev",
-              },
+            },
+            slidesPerView: 5,
         });
     }
     
     function swiperSliderLogos(){
         const swiper = new Swiper(".sliderLogos", {
+            spaceBetween: 30,
+            slidesPerView: 6,
+            pagination: {
+                el: ".sliderLogos .swiper-pagination",
+                clickable: true,
+              },
             navigation: {
-              nextEl: ".cont_slider_logos .swiper-button-next",
-              prevEl: ".cont_slider_logos .swiper-button-prev",
+                nextEl: ".cont_slider_logos .swiper-button-next",
+                prevEl: ".cont_slider_logos .swiper-button-prev",
+            },
+            grid: {
+                rows: 2,
             },
         });
     }
